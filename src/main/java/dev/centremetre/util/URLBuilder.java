cc53cccc02b96a9e5fdb93e9b748d8b46eff2d4a
CentreMetre <luckyminer01@gmail.com>
2025-07-07 01:21:27 +0100

    Implement URL builder

---
+++ src/main/java/dev/centremetre/util/URLBuilder.java
@@ -0,0 +1,107 @@
+package dev.centremetre.util;
+//Todo review if needed
+/**
+ * Use the builder pattern to build a URL with the 5 parameters.
+ */
+public class URLBuilder
+{
+    private static final String BASE_URL = "https://opentdb.com/api.php?";
+
+    private int questionCount;
+
+    private String questionType;
+
+    private String difficulty;
+
+    private String category;
+
+    private String encoding;
+
+    /**
+     *
+     * @param questionCount The amount of questions to request for. Must be between 1 and 50 (inclusive)
+     * @throws IllegalArgumentException If the amount is less than 1 or greater than 50.
+     * @throws NumberFormatException If the amount is not a number or if it is not an integer.
+     */
+    public URLBuilder(String questionCount)
+    {
+        setQuestionCount(questionCount);
+    }
+
+    /**
+     *
+     * @param questionCount The amount of questions to request for. Must be between 1 and 50 (inclusive)
+     * @throws IllegalArgumentException If the amount is less than 1 or greater than 50.
+     * @throws NumberFormatException If the amount is not a number or if it is not an integer.
+     */
+    private void setQuestionCount(String questionCount)
+    {
+        int intAmount = Integer.parseInt(questionCount);
+
+        if (intAmount < 1)
+        {
+            throw new IllegalArgumentException("Question count cannot be less than 1.");
+        }
+
+        if (intAmount > 50)
+        {
+            throw new IllegalArgumentException("Question count cannot be greater than 50.");
+        }
+
+        this.questionCount = intAmount;
+    }
+
+    public URLBuilder setQuestionType(String questionType)
+    {
+        this.questionType = questionType;
+        return this;
+    }
+
+    public URLBuilder setDifficulty(String difficulty)
+    {
+        this.difficulty = difficulty;
+        return this;
+    }
+
+    public URLBuilder setCategory(String category)
+    {
+        this.category = category;
+        return this;
+    }
+
+    public URLBuilder setEncoding(String encoding)
+    {
+        this.encoding = encoding;
+        return this;
+    }
+
+    /**
+     * Builds the URL made using the builder pattern on this class.
+     * @return A string which contains the URL to call.
+     */
+    public String build()
+    {
+        StringBuilder url = new StringBuilder();
+        url.append(BASE_URL);
+        url.append("amount=").append(this.questionCount);
+
+        if (this.questionType != null)
+        {
+            url.append("&type=").append(this.questionType);
+        }
+        if (this.difficulty != null)
+        {
+            url.append("&difficulty=").append(this.difficulty);
+        }
+        if (this.category != null)
+        {
+            url.append("&category=").append(this.category);
+        }
+        if (this.encoding != null)
+        {
+            url.append("&encode=").append(this.encoding);
+        }
+
+        return url.toString();
+    }
+}

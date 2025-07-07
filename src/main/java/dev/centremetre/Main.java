058ea4922696cbf809878d19411274713cf03ae1
CentreMetre <luckyminer01@gmail.com>
2025-07-07 01:18:51 +0100

    Main

---
+++ src/main/java/dev/centremetre/Main.java
@@ -0,0 +1,21 @@
+package dev.centremetre;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jdk.jshell.spi.ExecutionControl;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.lang.System.exit;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+    }
+}

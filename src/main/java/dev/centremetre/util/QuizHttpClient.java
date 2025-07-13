2dade4c3855458c2c30948f2828a0074065c361d
CentreMetre <luckyminer01@gmail.com>
2025-07-13 22:34:29 +0100

    Create HTTP call

---
+++ src/main/java/dev/centremetre/util/QuizHttpClient.java
@@ -0,0 +1,26 @@
+package dev.centremetre.util;
+
+import dev.centremetre.external.OpenTDBResponse;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class QuizHttpClient
+{
+    private static final HttpClient client = HttpClient.newHttpClient();
+
+    public static HttpResponse<String> callService(String url) throws IOException, InterruptedException
+    {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .build();
+
+        OpenTDBResponse quizResponse = new OpenTDBResponse();
+
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+}

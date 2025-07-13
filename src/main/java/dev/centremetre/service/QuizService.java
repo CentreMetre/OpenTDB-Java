64f407ea9e6a54a2d645af9ce20b4e0abe22a705
CentreMetre <luckyminer01@gmail.com>
2025-07-13 22:34:40 +0100

    Create QuizService class

---
+++ src/main/java/dev/centremetre/service/QuizService.java
@@ -0,0 +1,15 @@
+package dev.centremetre.service;
+
+import dev.centremetre.model.Quiz;
+import dev.centremetre.util.QuizHttpClient;
+import dev.centremetre.util.QuizUrlBuilder;
+
+public class QuizService
+{
+//    public Quiz createQuiz(QuizUrlBuilder quizUrlBuilder)
+//    {
+//        String url = quizUrlBuilder.build();
+//
+//        QuizHttpClient.callService(url);
+//    }
+}
